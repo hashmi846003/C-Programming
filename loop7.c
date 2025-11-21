61cf716a1b32cad8fc1808680619c15a8526ebcc
hashmi846003 <hashmi846001@gmail.com>
2025-11-21 08:59:43 +0530

    middle number

--- loop7.c
+++ loop7.c
@@ -1,11 +1,24 @@
 // using for loop for reversing an integer
 #include<stdio.h>
 int main(){
-    int num,rev=0,rem;
+    int num,rev=0,rem,copy,count=0;
     printf("Enter the number to be reversed\n");
     scanf("%d",&num);
+    copy = num;
+   // if (num==0) {
+   //     count=1;
+    //}
     for(;num;num/=10){
         rev=rev*10+num%10;
+        //count++;
     }
+    count++;
+    printf("Number of digits: %d\n", count);
     printf("Reversed number is %d\n",rev);
+    printf("Original number was %d\n",copy);
+    if (rev == copy){
+        printf("The number is a palindrome\n");
+    } else {
+        printf("The number is not a palindrome\n");
+    }
 }
