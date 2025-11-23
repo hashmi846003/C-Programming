82140d2c8b994f30b19bc95c6d7ff925837cb540
hashmi846003 <hashmi846001@gmail.com>
2025-11-23 13:30:55 +0530

    nested loop

---
+++ loop_assignment12.c
@@ -0,0 +1,20 @@
+//loop of characters in decrementing order
+#include <stdio.h>
+int main(){
+    int i,j,n;
+    char ch;
+    printf("Enter number of rows: ");
+    scanf("%d",&n);
+    for(i=n;i>=1;i--){
+        ch='A';
+        for(j=1;j<=i;j++){
+            printf("%c",ch++);
+           // ch++;
+          // j++;
+        }
+        //k++;
+        //ch++;
+        printf("\n");
+        }
+    return 0;
+}
