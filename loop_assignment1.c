d83bcfd10bec6c261a4632f3867947ed49e56dc9
hashmi846003 <hashmi846001@gmail.com>
2025-11-23 12:20:28 +0530

    assignment1

---
+++ loop_assignment1.c
@@ -0,0 +1,25 @@
+//printing in reverse incrementing triangle
+#include <stdio.h>
+
+int main() {
+    int rows;
+    printf("Enter number of rows: ");
+    scanf("%d", &rows);
+
+    for (int i = 1; i <= rows; i++) {
+
+        
+        for (int s = 1; s <= rows - i; s++) {
+            printf(" ");
+        }
+
+        
+        for (int j = 1; j <= i; j++) {
+            printf("*");
+        }
+
+        printf("\n");
+    }
+
+    return 0;
+}
