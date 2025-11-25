b45cbbfdd00292ac26b1378d3b06c98829f321d3
hashmi846003 <hashmi846001@gmail.com>
2025-11-25 08:05:08 +0530

    classwork

---
+++ fibonacci-series-pyramid.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+
+int main() {
+    int rows, i, j, space;
+    long long a = 0, b = 1, c;
+
+    printf("Enter number of rows: ");
+    scanf("%d", &rows);
+
+    for(i = 1; i <= rows; i++) {
+
+        
+        for(space = 1; space <= rows - i; space++)
+            printf("  ");
+
+        
+        for(j = 1; j <= i; j++) {
+            printf("%lld   ", a);
+            c = a + b;
+            a = b;
+            b = c;
+        }
+
+        printf("\n");
+    }
+
+    return 0;
+}
