650cfb21781d22efe17ab73eceeaef11af4a231b
Fakhruddin Hashmi <109304853+hashmi846003@users.noreply.github.com>
2025-12-24 00:36:02 +0530

    string_copy

--- my_string_copy1.c
+++ my_string_copy1.c
@@ -1,6 +1,18 @@
 //WAP to copy a string
 #include<stdio.h>
 #include<string.h>
+int main()
 {
-    
+    char str1[100];
+    char str2[100];
+    int i=0;
+    printf("Enter the string \n");
+    scanf(" %[^\n]", str1);
+    getchar();
+    while(str1[i]!='\0'){
+        str2[i]=str1[i];
+        i++;
+    }
+    str2[i]='\0';
+    printf("%s",str2);
 }
