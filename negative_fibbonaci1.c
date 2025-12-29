e78b65cb5191bc56b18dc2f41cb85fd1fef64151
hashmi846003 <hashmi846001@gmail.com>
2025-12-29 10:46:11 +0530

    negative fibbonaci

---
+++ negative_fibbonaci1.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int fib(int n)
+{
+    if (n == 0)
+        return 0;
+    if (n == 1)
+        return 1;
+
+    return fib(n - 1) + fib(n - 2);
+}
+
+int main()
+{
+    int n;
+    scanf("%d", &n);
+
+    // Case 1: zero
+    if (n == 0)
+    {
+        printf("0");
+        return 0;
+    }
+
+    // Case 2: positive input
+    if (n > 0)
+    {
+        printf("Invalid input");
+        return 0;
+    }
+
+    // Case 3: negative input
+    int limit = abs(n);
+    int i = 0;
+    int first = 1;
+
+    while (1)
+    {
+        int val = fib(i);
+
+        // Apply negafibonacci sign rule
+        if (i % 2 != 0)
+            val = -val;
+
+        if (abs(val) > limit)
+            break;
+
+        if (!first)
+            printf(", ");
+        printf("%d", val);
+
+        first = 0;
+        i++;
+    }
+
+    return 0;
+}
