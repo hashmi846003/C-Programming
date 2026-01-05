e7d4c8d121f912bc35b5d98470f6e1e9b473e965
Fakhruddin Hashmi <109304853+hashmi846003@users.noreply.github.com>
2026-01-06 01:55:51 +0530

    practice

--- strncat.c
+++ strncat.c
@@ -1,20 +1,42 @@
 #include <stdio.h>
 
 char *my_strncat(char *dst, const char *src, size_t n) {
-    char *ret = dst;
-    while (*dst) dst++;
+    int i = 0, j = 0;
 
-    while (n-- && *src) {
-        *dst++ = *src++;
+    
+    while (dst[i] != '\0') {
+        i++;
     }
-    *dst = '\0';
 
-    return ret;
+    
+    while (j < n && src[j] != '\0') {
+        dst[i] = src[j];
+        i++;
+        j++;
+    }
+
+    
+    dst[i] = '\0';
+
+    return dst;
 }
 
 int main() {
-    char buffer[50] = "Hello";
-    my_strncat(buffer, " Universe", 3);  
-    printf("Result: %s\n", buffer);      
+    char dst[200], src[100];
+    int n;
+
+    printf("Enter destination string: ");
+    scanf("%199s", dst);
+
+    printf("Enter source string: ");
+    scanf("%99s", src);
+
+    printf("Enter n (number of characters to append): ");
+    scanf("%d", &n);
+
+    my_strncat(dst, src, n);
+
+    printf("Result: %s\n", dst);
+
     return 0;
 }
