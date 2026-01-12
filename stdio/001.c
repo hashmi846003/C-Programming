65eb53ebb135ce0f9f1e76e8cf0684c165b4b624
hashmi846003 <hashmi846001@gmail.com>
2026-01-12 10:56:26 +0530

    stdio

---
+++ stdio/001.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<ctype.h>
+int main()
+{
+    char ch;
+    char str[]="EOF received";
+    while((ch=getchar()!=EOF)){
+        putchar(toupper(ch));
+    }
+    puts(str);
+}
