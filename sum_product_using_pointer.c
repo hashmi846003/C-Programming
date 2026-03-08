c764066fd97cafc3a0cb5b310c76641d71eb893b
Fakhruddin Hashmi <109304853+hashmi846003@users.noreply.github.com>
2026-03-08 10:10:38 +0530

    sum and product using pointer

---
+++ sum_product_using_pointer.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void sum_and_prod(int,int,*int,*int);
+int main(){
+    int n1,n2;
+    printf("Enter the two numbers\n");
+    scanf("%d %d",&n1,&n2);
+    int *p1=&n1;
+    int *p2=&n2;
+    sum_and_prod( n1,n2,*p1,*p2);
+}
+void sum_and_prod(int n1,int n2,int *p1,int *p2){
+     *p1=n1+n2;
+     *p2=n1*n2;
+    printf("sum and product is %d and %d", *p1,*p2);
+}
