c0c98716532c67bfc379f46a65ef81f08a914714
hashmi846003 <hashmi846001@gmail.com>
2026-03-12 10:56:00 +0530

    enums and typedef

---
+++ enum.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+typedef enum{
+    e_red,
+    e_blue,
+    e_green
+}color;
+int main(){
+    color e_white=0,e_black;
+    printf("%d\n",e_red);
+    printf("%d\n",e_blue);
+    printf("%d\n",e_green);
+    return 0;
+}
---
+++ typdef001.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+typedef double array_of_hundred[100];
+int main(){
+    array_of_hundred arr;
+    printf("%lu",sizeof(arr)); 
+}
